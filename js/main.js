922ab1b8822b256ebdc90594472cb144509a139f
Dongjun Yang <motokrzr11@gmail.com>
2024-12-14 16:10:32 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -130,14 +130,14 @@
 		let imgElem;
 		for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
 			imgElem = new Image();
-			imgElem.src = `./video/001/${1 + i}.JPG`;
+			imgElem.src = `../video/001/${1 + i}.JPG`;
 			sceneInfo[0].objs.videoImages.push(imgElem);
 		}
 
 		let imgElem2;
 		for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
 			imgElem2 = new Image();
-			imgElem2.src = `./video/002/IMG_${1000 + i}.JPG`;
+			imgElem2.src = `../video/002/IMG_${1000 + i}.JPG`;
 			sceneInfo[2].objs.videoImages.push(imgElem2);
 		}
 
